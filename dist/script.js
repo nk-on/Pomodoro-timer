b20ca1bec6930cf99d89604292b3edc0e7ed4da0
nk-on <2nkoz@proton.me>
2024-07-01 22:23:23 -0700

    Selecting variables from document object

--- dist/script.js
+++ dist/script.js
@@ -2,13 +2,13 @@
 const breakLengthContainer = document.querySelector('[data-role="break-length"]');
 const sessionLengthContainer = document.querySelector('[data-role="session-length"]');
 const clockElement = document.querySelector('[data-role="clock"]');
-const breakPlusSign = breakLengthContainer === null || breakLengthContainer === void 0 ? void 0 : breakLengthContainer.querySelector('[data-action="increase-break"]');
-const breakNumber = breakLengthContainer === null || breakLengthContainer === void 0 ? void 0 : breakLengthContainer.querySelector('[data-value="break-time"]');
-const breakMinusSign = breakLengthContainer === null || breakLengthContainer === void 0 ? void 0 : breakLengthContainer.querySelector('[data-action="decrease-break"]');
-const sessionPlusSign = sessionLengthContainer === null || sessionLengthContainer === void 0 ? void 0 : sessionLengthContainer.querySelector('[data-action="increase-session"]');
-const sessionNumber = sessionLengthContainer === null || sessionLengthContainer === void 0 ? void 0 : sessionLengthContainer.querySelector('[data-value="session-time"]');
-const sessionMinusSign = sessionLengthContainer === null || sessionLengthContainer === void 0 ? void 0 : sessionLengthContainer.querySelector('[data-action="decrease-session"]');
-const clockTime = clockElement === null || clockElement === void 0 ? void 0 : clockElement.querySelector('[data-time="clock"]');
+const breakPlusSign = document.querySelector('[data-action="increase-break"]');
+const breakNumber = document.querySelector('[data-value="break-time"]');
+const breakMinusSign = document.querySelector('[data-action="decrease-break"]');
+const sessionPlusSign = document.querySelector('[data-action="increase-session"]');
+const sessionNumber = document.querySelector('[data-value="session-time"]');
+const sessionMinusSign = document.querySelector('[data-action="decrease-session"]');
+const clockTime = document.querySelector('[data-time="clock"]');
 //user should be able to increase session length of decrease it (from 1 to 25)
 //when they click start time should pass in reverse
 class TimeMannager {
--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,13 @@
 const breakLengthContainer = document.querySelector<HTMLDivElement>('[data-role="break-length"]');
 const sessionLengthContainer = document.querySelector<HTMLDivElement>('[data-role="session-length"]');
 const clockElement = document.querySelector<HTMLDivElement>('[data-role="clock"]');
-const breakPlusSign = breakLengthContainer?.querySelector<HTMLSpanElement>('[data-action="increase-break"]');
-const breakNumber = breakLengthContainer?.querySelector<HTMLDivElement>('[data-value="break-time"]');
-const breakMinusSign = breakLengthContainer?.querySelector<HTMLSpanElement>('[data-action="decrease-break"]');
-const sessionPlusSign = sessionLengthContainer?.querySelector<HTMLSpanElement>('[data-action="increase-session"]');
-const sessionNumber = sessionLengthContainer?.querySelector<HTMLDivElement>('[data-value="session-time"]');
-const sessionMinusSign = sessionLengthContainer?.querySelector<HTMLSpanElement>('[data-action="decrease-session"]');
-const clockTime = clockElement?.querySelector<HTMLHeadingElement>('[data-time="clock"]');
+const breakPlusSign = document.querySelector<HTMLSpanElement>('[data-action="increase-break"]');
+const breakNumber =  document.querySelector<HTMLDivElement>('[data-value="break-time"]');
+const breakMinusSign = document.querySelector<HTMLSpanElement>('[data-action="decrease-break"]');
+const sessionPlusSign = document.querySelector<HTMLSpanElement>('[data-action="increase-session"]');
+const sessionNumber = document.querySelector<HTMLDivElement>('[data-value="session-time"]');
+const sessionMinusSign = document.querySelector<HTMLSpanElement>('[data-action="decrease-session"]');
+const clockTime = document.querySelector<HTMLHeadingElement>('[data-time="clock"]');
 //user should be able to increase session length of decrease it (from 1 to 25)
 //when they click start time should pass in reverse
 class TimeMannager {
